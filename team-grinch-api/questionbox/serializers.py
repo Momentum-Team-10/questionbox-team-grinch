5637803a3564380acacf54cbb658d4f330ffe319
keanya phelps <keanyaphelps@keanyas-MacBook-Air.local>
2021-12-18 16:04:53 -0600

    add usercreateserializer

--- team-grinch-api/questionbox/serializers.py
+++ team-grinch-api/questionbox/serializers.py
@@ -12,3 +12,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["pk", "username"]
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("username","email", "password")
