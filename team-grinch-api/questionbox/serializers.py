421c83a5ade25d112ed57aa87deac9a7516b01ae
keanya phelps <keanyaphelps@keanyas-MacBook-Air.local>
2021-12-18 15:57:27 -0600

    added user serializer

--- team-grinch-api/questionbox/serializers.py
+++ team-grinch-api/questionbox/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from .models import Question, User
+from .models import Question, User, Answer
 
 class QuestionSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Question
         fields = (
-            'pk', 'title', 'question', 'author', 'tags', 'favorited_by', 
+            'pk', 'title', 'body', 'author', 'tags', 'favorited_by', 
         )
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["pk", "username"]
